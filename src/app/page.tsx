d1f24fb48952a4c89ead0522cf5967ba0325b292
Mathias Lawson <mathiaslawson70@gmail.com>
2024-07-31 10:46:46 +0000

    integrate firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { db, auth } from '~/lib/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+
+interface Pantry{
+  name: string; 
+  quantity: number;
+}
+
 
 export default function Home() {
+  const [data, setData] = useState<Pantry | unknown>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'pantry'));
+      const dataList = querySnapshot.docs.map(doc => doc.data());
+      console.log(dataList, 'data list');
+      setData(dataList);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
