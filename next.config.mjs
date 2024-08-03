0c6c35bcb423ada7552acbbb5815dc42a070320b
Mathias Lawson <mathiaslawson70@gmail.com>
2024-08-03 21:52:07 +0000

    custom webpack config on next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        // Ignore node-specific modules when bundling for the browser
-        // https://webpack.js.org/configuration/resolve/#resolvealias
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            "sharp$": false,
-            "onnxruntime-node$": false,
-        }
-        return config;
+    experimental: {
+      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
     },
-};
-
+    webpack: (config) => {
+      // Ignore node-specific modules when bundling for the browser
+      // https://webpack.js.org/configuration/resolve/#resolvealias
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        sharp$: false,
+        "onnxruntime-node$": false,
+      };
+      return config;
+    },
+  }
 export default nextConfig;
